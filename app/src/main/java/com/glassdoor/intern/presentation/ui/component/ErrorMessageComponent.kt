c50283bc58fd3148bb12c2250eb6004e5696f82b
Paul Leung <78509075+PaulLeung93@users.noreply.github.com>
2024-01-21 16:26:32 -0500

    fix:In ErrorMessageComponent.kt: Defined how long error message will be displayed (3 seconds).     Defined background color, text color,style,alignment of the ErrorMessageComponent Composable function

--- app/src/main/java/com/glassdoor/intern/presentation/ui/component/ErrorMessageComponent.kt
+++ app/src/main/java/com/glassdoor/intern/presentation/ui/component/ErrorMessageComponent.kt
@@ -10,23 +10,28 @@
 package com.glassdoor.intern.presentation.ui.component
 
 import androidx.compose.animation.Crossfade
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import androidx.compose.ui.unit.sp
 import com.glassdoor.intern.presentation.theme.InternTheme
 import com.glassdoor.intern.utils.previewParameterProviderOf
 import kotlinx.coroutines.delay
 
 /**
- * TODO: Define how long the error message will be displayed
+ * DONE: Define how long the error message will be displayed
  */
-private const val SHOW_ERROR_MESSAGE_DURATION_IS_MILLIS: Long = 0L
+private const val SHOW_ERROR_MESSAGE_DURATION_IS_MILLIS: Long = 3000L
 
 @Composable
 internal fun ErrorMessageComponent(
@@ -40,21 +45,28 @@ internal fun ErrorMessageComponent(
 ) { state ->
     if (!state.isNullOrEmpty()) {
         /**
-         * TODO: Define the [background color](https://developer.android.com/jetpack/compose/modifiers#scope-safety), as well as [the color, style, and alignment](https://developer.android.com/jetpack/compose/text/style-text) of the error message
+         * DONE: Define the [background color](https://developer.android.com/jetpack/compose/modifiers#scope-safety), as well as [the color, style, and alignment](https://developer.android.com/jetpack/compose/text/style-text) of the error message
          */
         Text(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(InternTheme.dimensions.normal),
+                .padding(InternTheme.dimensions.normal)
+                .background(Color.White),
             text = state,
+            color = Color.Red,
+            style = TextStyle(
+                fontSize = 18.sp
+            ),
+            textAlign = TextAlign.Center
         )
 
         LaunchedEffect(key1 = errorMessage) {
             delay(SHOW_ERROR_MESSAGE_DURATION_IS_MILLIS)
 
             /**
-             * TODO: Call an action that hides the error message
+             * DONE: Call an action that hides the error message
              */
+            hideErrorMessageAction()
         }
     }
 }
